8c2e2b54cc8161219b18f23d7634fef14cefddb9
HansikaChathurangani <as20240951@sci.sjp.ac.lk>
2025-10-25 12:00:35 +0530

    Add Route class for path storage
    - Created Route inner class
    - Added route path storage
    - Added distance tracking
    - Created route display format
    - Added basic route operations

--- src/javaassigmnet01/RouteOptimizer.java
+++ src/javaassigmnet01/RouteOptimizer.java
@@ -7,6 +7,34 @@ public class RouteOptimizer {
     private final CityManager cityManager;
     private final DistanceManager distanceManager;
     
+    // Simple class to store route information
+    class Route {
+        List<String> cities;
+        int totalDistance;
+        
+        public Route() {
+            cities = new ArrayList<>();
+            totalDistance = 0;
+        }
+        
+        public Route(List<String> cities, int totalDistance) {
+            this.cities = new ArrayList<>(cities);
+            this.totalDistance = totalDistance;
+        }
+        
+        @Override
+        public String toString() {
+            StringBuilder path = new StringBuilder();
+            for (int i = 0; i < cities.size(); i++) {
+                path.append(cities.get(i));
+                if (i < cities.size() - 1) {
+                    path.append(" → ");
+                }
+            }
+            return String.format("Route: %s (Total Distance: %d km)", path.toString(), totalDistance);
+        }
+    }
+    
     public RouteOptimizer(CityManager cityManager, DistanceManager distanceManager) {
         this.cityManager = cityManager;
         this.distanceManager = distanceManager;
